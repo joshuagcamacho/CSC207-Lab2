dba95e91843a0e501d127fdba4146dea39873cd5
Joshua Camacho <shua.camacho@gmail.com>
2024-05-23 19:07:42 -0400

    Fixed Task 1 bug

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -4,11 +4,11 @@ public class DataTypes {
     // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long counter = 0;
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
-            s += x;
+            counter += x;
         }
-        return s;
+        return counter;
     }
 }
